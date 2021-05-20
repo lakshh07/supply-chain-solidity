3047d9062df5c712dae50dc57ca9cc770687b477
Lakshay Maini <62194911+lakshh07@users.noreply.github.com>
2021-05-20 19:06:32 +0530

    Add files via upload

---
+++ Ownable.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.6.0;
+
+contract Ownable {
+    address public _owner;
+
+    constructor() internal {
+        _owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(isOwner(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    function isOwner() public view returns (bool) {
+        return (msg.sender == _owner);
+    }
+}
